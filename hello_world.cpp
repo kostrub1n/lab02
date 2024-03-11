ccd182d86b0f09a6bf899321dfbc9e807e4638b0
kostrub1n <aqut653@gmail.com>
2024-03-11 17:17:09 +0300

    hello_world

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,11 @@
-
+#include <string>
 
 #include <iostream>
 using namespace std;
 
 int main(int argc, const char * argv[]) {
-    
+    string name;
+    cin >> name;
     cout << "Hello, World!\n";
     return 0;
 }
