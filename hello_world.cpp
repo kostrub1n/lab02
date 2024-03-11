5ad3c05f45917e569639ffa5c0bea428a653001f
kostrub1n <aqut653@gmail.com>
2024-03-11 17:20:14 +0300

    hello_world

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,10 @@
 #include <string>
-
 #include <iostream>
 using namespace std;
 
 int main(int argc, const char * argv[]) {
-    string name;
-    cin >> name;
+    string name;//щаыафолал
+    cin >> name;//ыарыоарыолр
     cout << "Hello, World!\n";
     return 0;
 }
