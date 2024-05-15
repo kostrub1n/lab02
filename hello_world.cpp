e4e33959319995d82a8ad32b9d3cd049a29398f2
kostrub1n <144800787+kostrub1n@users.noreply.github.com>
2024-05-15 22:41:40 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,18 +1,9 @@
 #include <iostream>
 #include <string>
-using namespace std;
 
-<<<<<<< HEAD
-int main(int argc, const char * argv[]) {
-    string name;
-    cin >> name;
-    cout << "Hello, World!\n";
-    return 0;
-=======
-int main(int argc, const char * argv[]) {
-    string name;
-    cin >> name;
-    cout << "Hello, World!\n";
-    return 0;
->>>>>>> 33b1096 (hello_world)
+int main() {
+  string name;                                // name
+  std::cin >> name;                           // input
+  std::cout << "hello from " << name << endl; // output
+  return 0;
 }
